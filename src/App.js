4cf7f1c1c4820d4a9e82b57723cb76ad864c1f40
Ester Cristina <ester.cristina.silva@accenture.com>
2019-10-04 10:03:16 -0300

    second commit (is a first)

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.svg';
+
+function App() {
+  return (
+    <div className="container">
+       <img src={logo} alt="Aircnc"/>
+
+       <div className="content">
+          <p>
+            Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa!
+          </p>
+
+          <form>
+            <label htmlFor="email">E-MAIL * </label>
+            <input 
+              id="email" 
+              type="email" 
+              placeholder="Seu melhor e-mail"
+            />
+          </form>
+       </div>
+    </div>
+  );
+}
+
+export default App;
