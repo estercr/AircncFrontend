01404549be73352de4f9090f29505da271fe5bda
Ester Cristina <ester.cristina.silva@accenture.com>
2019-10-04 17:33:28 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useState} from 'react';
+import api from './services/api'
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  function handleSubmit(event){
+    event.preventDefault()
+
+    api.post('/sessions')
+  }
+
   return (
     <div className="container">
-       <img src={logo} alt="Aircnc"/>
+      <img src={logo} alt="Aircnc" />
 
-       <div className="content">
-          <p>
-            Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa!
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa!
           </p>
 
-          <form>
-            <label htmlFor="email">E-MAIL * </label>
-            <input 
-              id="email" 
-              type="email" 
-              placeholder="Seu melhor e-mail"
-            />
-          </form>
-       </div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL * </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
